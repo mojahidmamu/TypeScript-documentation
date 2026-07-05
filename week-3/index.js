e32ae53582275043880154d1b5d68c2bf5b37c51
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-05 10:51:10 +0600

    added

---
+++ week-3/index.js
@@ -0,0 +1,44 @@
+"use strict";
+// Q1: Review: Async/Await কী?
+Object.defineProperty(exports, "__esModule", { value: true });
+// JavaScript-এ API Call, Database Query, File Read ইত্যাদি সময় নেয়।
+// এই ধরনের কাজকে Asynchronous Operation বলে।
+// Example: 
+async function getMessage() {
+    return "Hello TypeScript";
+}
+getMessage().then((message) => {
+    console.log(message);
+});
+// Number Example: 
+async function getNumber() {
+    return 42;
+}
+getNumber().then((number) => {
+    console.log(number);
+});
+// Q2: Review: Async/Await কীভাবে কাজ করে?
+// Async/Await হল JavaScript-এর একটি feature যা asynchronous code কে synchronous code-এর মতো লিখতে সাহায্য করে।
+// boolean Example:
+async function isEven(num) {
+    return num % 2 === 0;
+}
+isEven(4).then((result) => {
+    console.log(result);
+});
+// Object Example:
+async function getUser() {
+    return { name: "John Doe", age: 30 };
+}
+getUser().then((user) => {
+    console.log(user);
+});
+// Array Example:
+async function getNumbers() {
+    return [1, 2, 3, 4, 5];
+}
+getNumbers().then((number) => {
+    console.log(number);
+});
+// 
+//# sourceMappingURL=index.js.map
---
+++ week-3/index.ts
@@ -0,0 +1,54 @@
+// Q1: Review: Async/Await কী?
+
+// JavaScript-এ API Call, Database Query, File Read ইত্যাদি সময় নেয়।
+
+// এই ধরনের কাজকে Asynchronous Operation বলে।
+
+// Example: 
+async function getMessage() {
+    return "Hello TypeScript";
+}
+getMessage().then((message) => {
+    console.log(message);
+});
+
+
+// Number Example: 
+async function getNumber() {
+    return 42;
+}
+getNumber().then((number) => {
+    console.log(number);
+});
+
+
+// Q2: Review: Async/Await কীভাবে কাজ করে?
+
+// Async/Await হল JavaScript-এর একটি feature যা asynchronous code কে synchronous code-এর মতো লিখতে সাহায্য করে।
+
+// boolean Example:
+async function isEven(num: number): Promise<boolean> {
+    return num % 2 === 0;
+}
+isEven(4).then((result) => {
+    console.log(result);
+});
+
+// Object Example:
+async function getUser() {
+    return { name: "John Doe", age: 30 };
+}
+getUser().then((user) => {
+    console.log(user);
+});
+
+
+// Array Example:
+async function getNumbers() {
+    return [1, 2, 3, 4, 5];
+}
+getNumbers().then((number) => {
+    console.log(number);
+})
+
+// 
