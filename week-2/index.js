f46ee0f2cc314ffe3d84f2865168fff11683de1f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-07-05 09:48:38 +0600

    added

--- week-2/index.js
+++ week-2/index.js
@@ -1,27 +1,18 @@
+"use strict";
 // Q1: What is an Interface?
 // Interface হলো একটি blueprint (নকশা)।
 // এটি বলে দেয় একটি object-এর structure কেমন হবে।
 // Interface হলো একটি contract (চুক্তি)। এটি বলে দেয় কোন কোন properties এবং methods একটি object-এ থাকতে হবে।
-
-interface Student {
-    name: string;
-    age: number;
-    isStudent: boolean;
-}
-
-const student1: Student = {
+Object.defineProperty(exports, "__esModule", { value: true });
+const student1 = {
     name: "Mojahid",
     age: 20,
     isStudent: true,
 };
-
-const student2: Student = {
+const student2 = {
     name: "Rahim",
     age: 22,
     isStudent: false,
 };
-console.log(student1)
-
-
-
-
+console.log(student1);
+//# sourceMappingURL=index.js.map
